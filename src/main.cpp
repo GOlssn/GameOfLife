80c9137c7caa63201fe7f156e06dbb02c830747e
Gustav Olsson <golsson11@gmail.com>
2016-05-05 21:29:48 +0200

    * Changes in main.cpp Screen can now be used to create and draw in a terminal window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
+#include "screen.h"
 
 int main(char *args[]) {
+
+	Terminal terminal;
+	Screen screen(80, 24);
+	screen.fill(' ', TerminalColor(COLOR::BLACK, COLOR::WHITE));
+
+	screen.draw(terminal);
+
+	system("pause");
 	return 0;
 }
