3015432629e606415c7bb82f3a53a8398e3e7d2e
Gustav Olsson <golsson11@gmail.com>
2016-05-16 11:18:20 +0200

    * Changed draw interval to 500ms

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -88,7 +88,7 @@ void GameEngine::drawOnScreen(vector<vector<Cell*>> pCellMap) {
 	}
 
 	screen.draw(terminal);
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 }
 
 /**
