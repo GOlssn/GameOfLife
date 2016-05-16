f0c16e3ef57fdaa35d685c2dc4837edcaca6fa19
Daniel Jennebo <daje1400@student.miun.se>
2016-05-16 11:14:54 +0200

    *Minor changes to README.md

--- include/DanielRule.h
+++ include/DanielRule.h
@@ -6,10 +6,10 @@
 * - If a living cell has 4 or more neighbours it dies.
 * - If a dead cell has 2 or more neighbours where at least 2 of them is age 1 or more. The dead cell will live.
 * - Colors:
-*	- If age 1: COLOR YELLOW.
-*	- If age 2: COLOR CYAN.
-*	- If age 3: COLOR BLUE.
-*	- If age 4: COLOR RED.
+*	- If cell age 1: COLOR YELLOW.
+*	- If cell age 2: COLOR CYAN.
+*	- If cell age 3: COLOR BLUE.
+*	- If cell age 4: COLOR RED.
 */
 #ifndef DANIELRULE_H
 #define DANIELRULE_H
